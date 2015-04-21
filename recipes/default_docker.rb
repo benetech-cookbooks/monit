3ed6034b61740fdfef55c2c0aac3f978b3b8bbc3
Ron Ellis <rkeiii@gmail.com>
2015-04-21 12:19:31 -0700

    updates for tts class addition

---
+++ recipes/default_docker.rb
@@ -0,0 +1,25 @@
+package "monit"
+
+cookbook_file "/etc/default/monit" do
+  source "monit.default"
+  owner "root"
+  group "root"
+  mode 0644
+  only_if { platform?("ubuntu") || platform?("debian") }
+end
+
+
+directory "/etc/monit/conf.d/" do
+  owner  'root'
+  group 'root'
+  mode 0755
+  action :create
+  recursive true
+end
+
+template "/etc/monit/monitrc" do
+  owner "root"
+  group "root"
+  mode 0700
+  source 'monitrc.erb'
+end
